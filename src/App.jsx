62b059afa0d28956535c830e0f41c39c7c752ade
Nruzhzhaugash <clonn262@gmail.com>
2024-03-06 22:18:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="card">
+			<div className="ribbon">8 Марта</div>
+			<h1>С праздником весны!</h1>
+			<p>Дорогие женщины, пусть этот день принесет вам радость и улыбки!</p>
+			<img src="https://your-image-url.jpg" alt="Праздничное изображение" />
+			<div className="footer">С любовью, одногруппники</div>
+			<a href="https://2gis.kz/almaty/geo/70030076127331063" className="button">Отправить поздравление</a>
+		</div>
+  )
+}
+
+export default App
