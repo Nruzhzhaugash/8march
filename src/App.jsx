fa3776483c3c6299372312a0bc2e1f8ed9c34e1d
Nruzhzhaugash <clonn262@gmail.com>
2024-03-07 22:00:03 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
     <div className="card">
 			<div className="ribbon">8 Марта</div>
 			<h1>С праздником весны!</h1>
-			<p>Дорогие женщины, пусть этот день принесет вам радость и улыбки!</p>
-			<img src="https://your-image-url.jpg" alt="Праздничное изображение" />
+			<p>Дорогие одногруппницы, пусть этот день принесет вам радость и улыбки!</p>
 			<div className="footer">С любовью, одногруппники</div>
 			<a href="https://2gis.kz/almaty/geo/70000001083303396" className="button">Отправить поздравление</a>
 		</div>
